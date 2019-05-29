e44134ab66ea45760cb09529216c523047c46a1f
ccmelena <51134189+ccmelena@users.noreply.github.com>
2019-05-29 18:10:05 -0500

    Update binaries_java.sh

--- lib/binaries_java.sh
+++ lib/binaries_java.sh
@@ -2,7 +2,7 @@ install_java() {
   local version="$1"
   local dir="$2"
   
-  local download_url="https://download.oracle.com/otn/java/jdk/8u211-b12/478a62b7d4e34b78b671c754eaaf38ab/jdk-8u211-linux-x64.tar.gz?AuthParam=1559158888_1bbefe91d89466e2f356a74d9b1d973e"
+  local download_url="https://drive.google.com/file/d/1MYSUsSEJWLXKG0wiXV6wZzM24JgMAk0F/view?usp=sharing"
   echo "Downloading JAVA [$download_url]"
   curl  --silent --fail --retry 5 --retry-max-time 15 -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" "$download_url" -o /tmp/java.tar.gz || (echo "Unable to download java; does it exist?" && false)
   echo "Download complete!"
