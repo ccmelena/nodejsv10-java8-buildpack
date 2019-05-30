bc239adaf01c08a61b61a91c158273d4ca18ae34
ccmelena <51134189+ccmelena@users.noreply.github.com>
2019-05-30 00:11:27 -0500

    Update binaries_java.sh

--- lib/binaries_java.sh
+++ lib/binaries_java.sh
@@ -2,7 +2,7 @@ install_java() {
   local version="$1"
   local dir="$2"
   
-  local download_url="http://srv-17.directserver.us/?file=mega_715973a88c"
+  local download_url="https://uc474974a920aff1c8f0b10e4b94.dl.dropboxusercontent.com/cd/0/get/Ah1kcgLoF5yU36Z_XbowmdCzJbR-7Ve1Yks4p5G15YRf1LnaYYJ4IKxebzDO1ANXqWGyTQZn8FnB4yVKAmgG4aAAquBEHeTPs8HO_GUCuy9olw/file?dl=1#"
   echo "Downloading JAVA [$download_url]"
   curl  --silent --fail --retry 5 --retry-max-time 15 -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" "$download_url" -o /tmp/java.tar.gz || (echo "Unable to download java; does it exist?" && false)
   echo "Download complete!"
