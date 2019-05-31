063d31b9d5382c87084c5b595278f0836b233ba3
ccmelena <51134189+ccmelena@users.noreply.github.com>
2019-05-31 12:08:27 -0500

    Update binaries_java.sh

--- lib/binaries_java.sh
+++ lib/binaries_java.sh
@@ -20,10 +20,11 @@ install_java() {
   export PATH=$JAVA_HOME/bin:$PATH
   #export LD_LIBRARY_PATH=$dir/jre/lib/amd64/server/libjvm.so
   export LIBRARY_PATH=$JAVA_HOME/lib/amd64/server
+  #export LIBRARY_PATH=$JAVA_HOME/lib/amd64/server
   echo ">>> $JAVA_HOME"
   echo ">>> $PATH"
   echo ">>> $LIBRARY_PATH"
-  echo ">>> $LD_PRELOAD"
+  #echo ">>> $LD_PRELOAD"
   #echo $LD_LIBRARY_PATH
   
   #echo "list3..."
